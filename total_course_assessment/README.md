a1627a62dd8ced8846c84f06b7e16eed9879d78d
Sebastien Siva <ssiva@ggc.edu>
2024-05-02 13:57:14 -0400

    Command Line Version Updated as Class.

--- total_course_assessment/README.md
+++ total_course_assessment/README.md
@@ -1,3 +1,29 @@
+# total course assessment
+
+The script used 3 types of files to calculate course goal scores for a D2L section. Course goal scores are the percentage of students getting a 70% or better on course goal related assessment.
+
+* *File_Type1** (>= 0 files): Quiz grade export CSV files with Question Titles containing 1 or more course goal identifiers (like 'G3 G4 strings and if statements')
+
+* *File_Type2* (0 or 1 file): The D2L gradebook exported as Points CSV with OrgId, FirstName, LastName.
+
+* *File_Type3* (1 file only if File Type2 present): CSV file mapping grades to course goals according to the following format:
+
+GRADE_NAME,CG,CG,CG,CG,CG  
+final_p1,5,6,1  
+final_p2,5,6
+
+
+1.  Feed this script 0 command line arguments and it will automatically search the directory for the files described above.
+
+2.  Optionally include the files explicitly as command line arguments.
+
+Ex: python3 cg_score.py path_to_quz1_cg.csv path_to_quz2_cg.csv path_to_gradebook.csv path_to_grade_cg_map.csv
+
+Ex: python3 cg_score.py 2140_grade_cg.csv GB_Points.csv FinalExamMC.csv
+
+
+[Video Tutorial](https://ggcedu-my.sharepoint.com/:v:/g/personal/ssiva_ggc_edu/EVO5HIB7c0dNhDJctyECUpEBp-8Sq5dzMNLDvScqeoeuOw?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=EXA3vG)
+
 # How to build
 
 Build with
--- total_course_assessment/cg_score.py
+++ total_course_assessment/cg_score.py
@@ -3,117 +3,210 @@ from sys import *
 from collections import defaultdict
 import os, csv, re
 
-grade_book_style_str = "Points Grade <Numeric MaxPoints"
-quiz_grade_style_str = "Q Title"
+GRADE_BOOK_SEARCH_TAG = "Points Grade <Numeric MaxPoints"
+QUIZ_GRADE_SEARCH_TAG = "Q Title"
 
-directions = """
-1.  Feed this script 0 or more D2L quiz grade export CSV files with Question Titles containing 1 or more course goal identifiers (like 'G3 G4 strings and if statements') 
-2.  Optionally include the D2L gradebook (exported as Points CSV with OrgId, FirstName, LastName) and a separate CSV file mapping grades to course goals according to the following format:
+CMD_LINE_HELP = """
+The script used 3 types of files to calculate course goal scores for a D2L section. Course goal scores are the percentage of students getting a 70% or better on course goal related assessment.
+
+File_Type1* (>= 0 files): Quiz grade export CSV files with Question Titles containing 1 or more course goal identifiers (like 'G3 G4 strings and if statements')
+
+File_Type2 (0 or 1 file): The D2L gradebook exported as Points CSV with OrgId, FirstName, LastName.
+
+File_Type3 (1 file only if File Type2 present): CSV file mapping grades to course goals according to the following format:
 
 GRADE_NAME,CG,CG,CG,CG,CG
 final_p1,5,6,1
 final_p2,5,6
 
+Feed this script 0 command line arguments and it will automatically search the directory for the files described above.
+
+Optionally include the files explicitly as command line arguments.
+
 Ex: python3 cg_score.py path_to_quz1_cg.csv path_to_quz2_cg.csv path_to_gradebook.csv path_to_grade_cg_map.csv
 
 Ex: python3 cg_score.py 2140_grade_cg.csv GB_Points.csv FinalExamMC.csv
-
 """
 
 def fail(s):
-	print("ERORR:", s)
+	print(s)
 	exit(0)
 
-#########################################################################################
-# QUIZ FILES
-#########################################################################################
-def isQuizPointFile(f):
-	with open(f, 'r', newline='') as csvfile:
-		reader = csv.reader(csvfile)
-		first_row = next(reader)
-		for entry in first_row:
-			if entry == quiz_grade_style_str:
-				return True
-	return False;	
 
-def buildQuizCGPoints(f):
-	# print(f"Processing quiz: {f}")
-	quiz_points = {} # {'400.139102':{'CG1_points':17, 'CG1_max':20, 'CG2_points':11,}}
-	ques_titles = {} # {'400.139102':{'G5-O6/O7-a1', 'G2   valid var', ...}}
-	with open(f, 'r', newline='') as csvfile:
-		dr = csv.DictReader(csvfile)
-		for row in dr:
-			sid = '#%s-%s_%s' % (row['Org Defined ID'], row['FirstName'], row['LastName'])
-			if "400" not in sid: continue # avoid demo id
+class CG_Score:
 
-			if sid not in quiz_points:
-				quiz_points[sid] = {}
-				ques_titles[sid] = set()
-			student_points = quiz_points[sid]
-			student_questions = ques_titles[sid]
-			q_title = row['Q Title']+row['Q Text']
-			if q_title in student_questions: continue # question already done
-			student_questions.add(q_title)
+	def __init__(self):
+		self.grade_book = None
+		self.grade_book_cgs = None
+		self.students = defaultdict(lambda: defaultdict(int)) # {'#400.139102':{'CG1_points':17, 'CG1_max':20, ...}, '#400.222222':{'CG1_points':12, ...}}
+		self.ignored_students = defaultdict(list) # {'#400.139102':['Zero for Asg1', 'Zero for Asg2'], ...}
+		self.num_students_pass = defaultdict(int) # {CG1:13, CG2:14, ...}
+
+		
+	#################################################################################
+	# QUIZ FILES
+	#################################################################################
+	def isQuizPointFile(self, f):
+		with open(f, 'r', newline='') as csvfile:
+			reader = csv.reader(csvfile)
+			first_row = next(reader)
+			for entry in first_row:
+				if entry == QUIZ_GRADE_SEARCH_TAG:
+					return True
+		return False;	
+
+	def processQuizPointFile(self, f):
+		CG_points = {} # {'400.139102':{'CG1_points':17, 'CG1_max':20, 'CG2_points':11,}}
+		ques_titles = {} # {'400.139102':{'G5-O6/O7-a1', 'G2   valid var', ...}}
+		with open(f, 'r', newline='') as csvfile:
+			dr = csv.DictReader(csvfile)
+			for row in dr:
+				sid = '#%s-%s_%s' % (row['Org Defined ID'], row['FirstName'], row['LastName'])
+				if "400" not in sid: continue # avoid demo id
+
+				if sid not in CG_points:
+					CG_points[sid] = {}
+					ques_titles[sid] = set()
+				student_points = CG_points[sid]
+				student_questions = ques_titles[sid]
+				q_title = row['Q Title']+row['Q Text']
+				if q_title in student_questions: continue # question already done
+				student_questions.add(q_title)
 			
-			for cg in re.findall(r"G\d", q_title):
-				cgp = 'C' + cg + '_points'
-				cgm = 'C' + cg + '_max'
-				if cgp not in student_points:
-					student_points[cgp] = 0.0
-					student_points[cgm] = 0.0
-				student_points[cgp] += float(row['Score'])
-				student_points[cgm] += float(row['Out Of']) if row['Bonus?'].upper() == 'FALSE' else 0
-				
-	return quiz_points;
+				for cg in re.findall(r"G\d", q_title):
+					cgp = 'C' + cg + '_points'
+					cgm = 'C' + cg + '_max'
+					if cgp not in student_points:
+						student_points[cgp] = 0.0
+						student_points[cgm] = 0.0
+					student_points[cgp] += float(row['Score'])
+					student_points[cgm] += float(row['Out Of']) if row['Bonus?'].upper() == 'FALSE' else 0
 
 
-#########################################################################################
-# GRADEBOOK FILE
-#########################################################################################
+		self.check_student_mismatch(CG_points.keys(), f)
 
-def isGradeBookFile(f):
-	with open(f, 'r', newline='') as csvfile:
-		reader = csv.reader(csvfile)
-		first_row = next(reader)
-		for entry in first_row:
-			if grade_book_style_str in entry:
+		for sid in CG_points:
+			student_points = CG_points[sid]
+			for key in student_points:
+				self.students[sid][key] += student_points[key]
+
+
+	#################################################################################
+	# GRADEBOOK FILE
+	#################################################################################
+	def isGradeBookFile(self, f):
+		with open(f, 'r', newline='') as csvfile:
+			reader = csv.reader(csvfile)
+			first_row = next(reader)
+			for entry in first_row:
+				if GRADE_BOOK_SEARCH_TAG in entry:
+					return True
+		return False;	
+
+	def buildGradeBook(self, f):
+		gradebook = {} # {'#400.139102':{'copyme_asg2.1 Points Grade <Numeric MaxPoints:10 Weight:1.08 Category:Assignments CategoryWeight:30>':8, ...}}
+		with open(f, 'r', newline='') as csvfile:
+			dr = csv.DictReader(csvfile)
+			for row in dr:
+				sid = '%s-%s_%s' % (row['OrgDefinedId'], row['First Name'], row['Last Name'])			
+				if "400" not in sid: continue # avoid demo id (#)
+				gradebook[sid] = {}
+				for key in row:
+					if GRADE_BOOK_SEARCH_TAG in key:
+						gradebook[sid][key] = row[key]
+		
+		self.check_student_mismatch(gradebook.keys(), f)
+		return gradebook;
+
+	def processGradeBook(self):
+		if bool(self.grade_book) != bool(self.grade_book_cgs):
+			return "ERROR: Requires both gradebook and gradebook_cg_map or neither."
+		elif self.grade_book == None: return ""
+		
+		for sid in self.grade_book:
+			self.students[sid] = defaultdict(int)
+			for header in self.grade_book[sid]:
+				if GRADE_BOOK_SEARCH_TAG in header:
+					grade_name = header[0:header.find(GRADE_BOOK_SEARCH_TAG) - 1]
+					if grade_name in self.grade_book_cgs:
+						r = re.search(r"MaxPoints:(\d+)", header)
+						if not r: return ("ERROR: MaxPoints not found in", header)
+						grade_max = float(r.group(1))
+						grade_points = float(self.grade_book[sid][header])
+						if grade_points == 0:
+							self.ignored_students[sid].append('Zero for ' + grade_name)
+						for cg in self.grade_book_cgs[grade_name]:
+							self.students[sid][cg+'_points'] += grade_points
+							self.students[sid][cg+'_max'] += grade_max
+		return ""		
+
+
+	#########################################################################################
+	# GRADE BOOK CG MAPPING FILE
+	#########################################################################################
+	def isGradeBookCGMapFile(self, f):
+		with open(f, 'r', newline='') as csvfile:
+			reader = csv.reader(csvfile)
+			first_row = next(reader)
+			if first_row[0:2] == ["GRADE_NAME", "CG"]:
 				return True
-	return False;	
+		return False;	
 
-def buildGradeBook(f):
-	gradebook = {} # {'#400.139102':{'copyme_asg2.1 Points Grade <Numeric MaxPoints:10 Weight:1.08 Category:Assignments CategoryWeight:30>':8, ...}}
-	with open(f, 'r', newline='') as csvfile:
-		dr = csv.DictReader(csvfile)
-		for row in dr:
-			sid = '%s-%s_%s' % (row['OrgDefinedId'], row['First Name'], row['Last Name'])			
-			if "400" not in sid: continue # avoid demo id (#)
-			gradebook[sid] = {}
-			for key in row:
-				if grade_book_style_str in key:
-					gradebook[sid][key] = row[key]
-	return gradebook;
+	def buildGradeBookCGs(self, f):
+		gradebook_cgs = {}
+		with open(f, 'r', newline='') as csvfile:
+			reader = csv.reader(csvfile); next(reader)
+			for row in reader:
+				gradebook_cgs[row[0]] = ['CG' + x for x in row[1:]]
+		return gradebook_cgs
+		
 
+	#########################################################################################
+	# FILE ANALYSYS
+	#########################################################################################
+		
+	def process_file(self, f):
+		if self.isGradeBookFile(f):
+			if(not self.grade_book):	
+				self.grade_book = self.buildGradeBook(f)
+				return "GRADE BOOK"
+			else: return "ERROR: 2 Grade Book Files"
+		elif self.isGradeBookCGMapFile(f):
+			if(not self.grade_book_cgs):	
+				self.grade_book_cgs = self.buildGradeBookCGs(f)
+				return "GRADE BOOK COURSE GOAL MAP"
+			else: return ("ERROR: 2 Grade Book Course Goal Mapping Files")
+		elif self.isQuizPointFile(f):
+			self.processQuizPointFile(f)
+			return "QUIZ FILE"
+		else: return "ERROR: UNRECOGNISED FILE"
+	
 
-#########################################################################################
-# GRADE BOOK CG MAPPING FILE
-#########################################################################################
-def isGradeBookCGFile(f):
-	with open(f, 'r', newline='') as csvfile:
-		reader = csv.reader(csvfile)
-		first_row = next(reader)
-		if first_row[0:2] == ["GRADE_NAME", "CG"]:
-			return True
-	return False;	
-
-def buildGradeBookCGs(f):
-	gradebook_cgs = {}
-	with open(f, 'r', newline='') as csvfile:
-		reader = csv.reader(csvfile); next(reader)
-		for row in reader:
-			gradebook_cgs[row[0]] = ['CG' + x for x in row[1:]]
-	return gradebook_cgs
-
-
+	def check_student_mismatch(self, new_students, f_name):
+		old_students = self.students.keys()
+		if len(old_students) == 0:
+			return
+		# print("SPECIAL", new_students - old_students)
+		for sid in new_students - old_students:
+			self.ignored_students[sid].append("Unexpected new student " + sid + " found in " + f_name)
+		for sid in old_students - new_students:
+			self.ignored_students[sid].append("Student " + sid + " missing in " + f_name)
+	
+					
+	def filter_students(self):
+		filtered_students = {k:v for k, v in self.students.items() if k not in self.ignored_students}
+		self.students = filtered_students
+	
+	def calc_num_students_pass(self):
+		for scores in self.students.values():
+			for key in scores:
+				if '_points' in key:
+					points = scores[key]
+					max_points = scores[key.replace('_points', '_max')]
+					if max_points > 0 and points / max_points >= 0.7:
+						self.num_students_pass[key[0:-len('_points')]] += 1;
+	
+		
 
 #########################################################################################
 # MAIN
@@ -127,90 +220,44 @@ if __name__ == "__main__":
 			if os.path.isfile(f) and f[-4:].upper() == '.CSV':
 				file_names.append(f)
 	
-	grade_book = None
-	grade_book_cgs = None
-	quiz_cg_points = []
-
+	cgs = CG_Score()
+	
 	# search through argv files.
 	for f in file_names:
-		if isGradeBookFile(f):
-			if(not grade_book):	
-				grade_book = buildGradeBook(f)
-				print(f"USING {f} AS GRADE BOOK")
-			else: fail("2 grade book files.")
-		elif isGradeBookCGFile(f):
-			if(not grade_book_cgs):	
-				grade_book_cgs = buildGradeBookCGs(f)
-				print(f"USING {f} AS GRADE BOOK COURSE GOAL MAP")
-			else: fail("2 grade book course goal mapping files.")
-		elif isQuizPointFile(f):
-			print(f"USING {f} AS A QUIZ FILE")
-			quiz_cg_points.append(buildQuizCGPoints(f))
-		else: print(f"NOTE SKIPPING: {f} is unrecognized file.")
-
-	if bool(grade_book) != bool(grade_book_cgs):
-		fail("Need both gradebook and gradebook_cg_map or neither.")
-
+		r = cgs.process_file(f)
+		if r.startswith("ERROR"): fail(r)
+		print(r, "FILE:", f)
 	print()
 	
-	# BUILD STUDENT COURSE GOAL MAP (initialize on the fly if no gradebook given)
-	students = defaultdict(lambda: defaultdict(int)) # {'#400.139102':{'CG1_points':17, 'CG1_max':20, ...}, '#400.222222':{'CG1_points':12, ...}}
-	skip_students = set()
-
-	# GRADE BOOK
-	if grade_book:
-		for sid in grade_book:
-			students[sid] = defaultdict(int)
-			for header in grade_book[sid]:
-				if grade_book_style_str in header:
-					grade_name = header[0:header.find(grade_book_style_str) - 1]
-					if grade_name in grade_book_cgs:
-						r = re.search(r"MaxPoints:(\d+)", header)
-						if not r: fail("MaxPoints not found in", header)
-						grade_max = float(r.group(1))
-						grade_points = float(grade_book[sid][header])
-						if grade_points == 0:
-							if input(sid + " has zero for " + grade_name + 
-								".\nType 'y' to remove: ") == 'y': 
-								skip_students.add(sid)
-								break
-						for cg in grade_book_cgs[grade_name]:
-							students[sid][cg+'_points'] += grade_points
-							students[sid][cg+'_max'] += grade_max
+	r = cgs.processGradeBook()
+	if r.startswith("ERROR"): fail(r)
 	
-	# QUIZZES
-	for quiz in quiz_cg_points:
-		for sid in quiz:
-			if sid in skip_students: continue
-			student_points = quiz[sid]
-			for key in student_points:
-				students[sid][key] += student_points[key]
+	unignore = []
+	for sid in cgs.ignored_students:
+		print("Ignored Student: " + sid + " has:")
+		for reason in cgs.ignored_students[sid]:
+			print('\t' + reason)
+			if input("Ignore " + sid + "for analysis ([y] or n)? ") == 'n':
+				unignore.append(sid)
+	for sid in unignore:
+		del cgs.ignored_students[sid]
 	
-	filtered_students = {k:v for k, v in students.items() if k not in skip_students}
+	cgs.filter_students()
 
-
-	num_students_pass = defaultdict(int) # {CG1:13, CG2:14, ...}
-	for scores in filtered_students.values():
-		for key in scores:
-			if '_points' in key:
-				points = scores[key]
-				max_points = scores[key.replace('_points', '_max')]
-                                # Bonus questions can result in max points of 0
-				if max_points > 0 and points / max_points >= 0.7:
-					num_students_pass[key[0:-len('_points')]] += 1;
+	cgs.calc_num_students_pass()
 	
 	print("\nALL STUDENT COURSE GOAL SCORES")
-	for sid, points in filtered_students.items():
-		cgs = [c.replace('_points', '') for c in sorted(points.keys()) if '_points' in c]
+	for sid, points in cgs.students.items():
+		cg_names = [c.replace('_points', '') for c in sorted(points.keys()) if '_points' in c]
 		cg_scores = ["%s %s/%s" % (c, round(points[c + '_points'], 1), points[c + '_max']) 
-			for c in cgs]
+			for c in cg_names]
 		print(sid[sid.find('-')+1:], ", ".join(cg_scores)) # [(cg, points[cg]) for cg in cgs])
 	
 	
 	print("\nPERCENT OF STUDENTS GETTING >= 70% ON EACH COURSE GOAL")
-	for cg in sorted(num_students_pass.keys()):
-		p = round(100 * num_students_pass[cg] / len(filtered_students), 0)
+	for cg in sorted(cgs.num_students_pass.keys()):
+		p = round(100 * cgs.num_students_pass[cg] / len(cgs.students), 0)
 		print(cg + ": " + str(p) + '%')
 	
-	print("\nTOTAL STUDENTS:", len(filtered_students)) 
+	print("\nTOTAL STUDENTS:", len(cgs.students)) 
 			
